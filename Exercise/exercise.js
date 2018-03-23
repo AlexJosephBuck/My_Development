77a4c242f57b562b5f7cf338edff4e66a39ed792
Bucky <alexjosephbuck@gmail.com>
2018-03-23 14:42:26 -0400

    update

---
+++ Exercise/exercise.js
@@ -0,0 +1,30 @@
+// Exercise 1:
+const name = "Alex";
+console.log("Hello " + name);
+
+//Exercise 2:
+const number = 1125;
+const answer = number % 2;
+console.log(answer);
+
+//Exercise 3:
+const a = true;
+const b = false;
+const c = true;
+console.log(a || b || c);
+
+//Exercise 4:
+console.log( a && b && c);
+
+//Exercise 5:
+const firstAnswer = 10 - 3 * 5/4 + 202 % 3;
+const secondAnswer = 10 - (3 * (5/4) + (202 % 3));
+console.log(firstAnswer, secondAnswer);
+
+//Exercise 6:
+
+console.log(String(12));
+console.log(Number("113"));
+console.log(Number("1.12356"));
+var stringConvert = Number("Hello World");
+ console.log(stringConvert);
